3dc183ae5c72ed526676da8f4e7a2dbed6464d94
geonwoomun <ansejrrhkd@naver.com>
2020-02-04 18:00:22 +0900

    폴더들 src에 정리

---
+++ src/apis/apiKey.js
@@ -0,0 +1,5 @@
+export const apikeys = {
+    googleCX : "002682827247018715690:l1ekil5dsw2",
+    googleKey : "AIzaSyA6B0lw499awfUoG8dtLadXXaIfude_OdI",
+    youtubeKey : "AIzaSyA6B0lw499awfUoG8dtLadXXaIfude_OdI"
+}
