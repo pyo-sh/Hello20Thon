71b7c0d054976b4bd02e0f158eac37c8a0cde8a7
geonwoomun <ansejrrhkd@naver.com>
2020-02-07 13:16:12 +0900

    node 버전 명시 및 apikey...

---
+++ src/apis/apiKey.js
@@ -0,0 +1,5 @@
+export const apikeys = {
+  googleCX: "002682827247018715690:l1ekil5dsw2",
+  googleKey: "AIzaSyD5HCZ9juWYohmGB0ydUifzt7CN4FXuZXQ",
+  youtubeKey: "AIzaSyD5HCZ9juWYohmGB0ydUifzt7CN4FXuZXQ"
+};
