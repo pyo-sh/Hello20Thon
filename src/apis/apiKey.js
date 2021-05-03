3db06d16a56044fec5bb430dbc5c7b6bd3c087dd
pyo-sh <pygosky@gmail.com>
2021-05-04 06:10:43 +0900

    Temp ApiKey evironment for Vercel

---
+++ src/apis/apiKey.js
@@ -0,0 +1,5 @@
+export const apiKeys = {
+    youtubeKey: process.env.NEXT_PUBLIC_YOUTUBE_KEY,
+    googleCX: process.env.NEXT_PUBLIC_GOOGLE_CX,
+    googleKey: process.env.NEXT_PUBLIC_GOOGLE_KEY,
+}
